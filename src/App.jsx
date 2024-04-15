189dab7baba9f7c1f9608eb3c159d4cc0e7e18d4
dselmer <dselmer@icstars.org>
2024-04-15 14:29:51 -0500

    fished modual 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import Spinner from './Spinner'
+import useFetch from "./services/useFetch.js";
 
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
-
+  const [shoeSize, setShoeSize] = useState('');
+  const { data:products, loading, error } = useFetch("products?category=shoes");
+ 
+function renderProduct(p) {
   return (
-    <>
-      <div className="content">
-        <Header />
-        <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-        </main>
-      </div>
-      <Footer />
-    </>
+    <div key={p.id} className="product">
+      <a href="/">
+        <img src={`/images/${p.image}`} alt={p.name} />
+        <h3>{p.name}</h3>
+        <p>${p.price}</p>
+      </a>
+    </div>
   );
+}
+
+const filteredProducts = shoeSize ? products.filter(product => product.skus.find(sku => sku.size === parseInt(shoeSize))) :
+  products;
+
+if (error) throw error;
+if (loading) return <Spinner />
+return (
+  <>
+    <div className="content">
+      <Header />
+      <main>
+        <section id="filters">
+          <label htmlFor="size">Filter by Size:</label>{" "}
+          <select
+            id="size"
+            value={shoeSize}
+            onChange={(e) => setShoeSize(e.target.value)}
+          >
+            <option value="">All sizes</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+          {shoeSize && <h2>found {filteredProducts.length} items </h2>}
+        </section>
+        <section id='products'>
+          {filteredProducts.map(renderProduct)}
+        </section>
+      </main>
+    </div>
+    <Footer />
+  </>
+);
 }
